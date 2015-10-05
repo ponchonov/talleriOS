23e2c91890e9bb542e79cb0a03875cfa65aa31b8
ponchonov <poncho_nov@hotmail.com>
2015-10-05 13:57:26 -0500

    first commit

---
+++ taller/StudentDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudentDetailViewController.h
+//  taller
+//
+//  Created by HECTOR ALFONSO on 05/10/15.
+//  Copyright © 2015 birghtCoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentDetailViewController : UIViewController
+
+@end
---
+++ taller/StudentDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  StudentDetailViewController.m
+//  taller
+//
+//  Created by HECTOR ALFONSO on 05/10/15.
+//  Copyright © 2015 birghtCoders. All rights reserved.
+//
+
+#import "StudentDetailViewController.h"
+
+@interface StudentDetailViewController ()
+
+@end
+
+@implementation StudentDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
